935e78a2677f7b76a6a9a8e5188dd4cd551625d4
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-06 22:59:08 -0300

    cambio margin label

--- frontend/src/components/input_components/Label.vue
+++ frontend/src/components/input_components/Label.vue
@@ -1,5 +1,5 @@
 <template>
-    <label :for="props.for" class="block text-sm font-medium leading-6 text-gray-900"><slot/>
+    <label :for="props.for" class="block text-sm font-medium leading-6 text-gray-900 mb-1"><slot/>
         <span v-if="props.required" class="text-red-600">*</span></label>
 </template>
 
