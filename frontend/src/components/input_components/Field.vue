a387e047465d284c90ed30ef7b58c645574469f5
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-07 20:38:34 -0300

    cambios en componente Field - leftLabelSlot - añadido descripciones a los props

--- frontend/src/components/input_components/Field.vue
+++ frontend/src/components/input_components/Field.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-        <Label 
-            v-if="props.label"
-            :for="props.id"
-            :required="props.required"
-        >
-            {{ props.label }}
-        </Label>
-        <slot/>
+        <div class="flex items-center justify-between">
+            <Label v-if="props.label" :for="props.id" :required="props.required">
+                {{ props.label }}
+            </Label>
+            <div v-if="props.leftLabelSlot" class="text-sm">
+                <template v-if="typeof props.leftLabelSlot === 'string'">
+                    <p>{{ props.leftLabelSlot }}</p>
+                </template>
+                <template v-else-if="props.leftLabelSlot.href">
+                    <router-link :to="props.leftLabelSlot.href" :class="props.leftLabelSlot.class">
+                        {{ props.leftLabelSlot.text }}
+                    </router-link>
+                </template>
+                <template v-else>
+                    <p :class="[props.leftLabelSlot.class ? props.leftLabelSlot.class : '']">
+                        {{ props.leftLabelSlot.text }}
+                    </p>
+                </template>
+            </div>
+        </div>
+        <slot />
         <ErrorMessage v-if="props.error">
             {{ props.error }}
         </ErrorMessage>
@@ -16,36 +29,53 @@
         </HelperMessage>
     </div>
 </template>
-
+  
 <script setup>
-import { computed, provide } from 'vue';
+import { defineProps, computed, provide } from 'vue';
 import ErrorMessage from './ErrorMessage.vue';
 import HelperMessage from './HelperMessage.vue';
-
 import Label from './Label.vue';
-import { v4 as uuid } from 'uuid'
+import { v4 as uuid } from 'uuid';
 
-const props = defineProps({	
-    id : {
-        type:String,
-        default:() => `field-${uuid()}`
-    },	
-    label : String,	
-    error : String,	
-    help : String,	
-    required : Boolean,	
-})
+const props = defineProps({
+    id: {
+        type: String,
+        default: () => `field-${uuid()}`,
+        description: 'Unique identifier for the field.'
+    },
+    label: {
+        type: String,
+        description: 'Main label for the field.'
+    },
+    error: {
+        type: String,
+        description: 'Error message associated with the field.'
+    },
+    help: {
+        type: String,
+        description: 'Help message for the field.'
+    },
+    required: {
+        type: Boolean,
+        default: false,
+        description: 'Indicates whether the field is required.'
+    },
+    leftLabelSlot: {
+        type: [Object, String],
+        default: null,
+        description: 'Content for the left label slot or configuration object.'
+    }
+});
 
-
-const ariaDescribeBy = computed(()=>{
+const ariaDescribeBy = computed(() => {
     return !!props.help ? `help-${uuid()}` : null;
-})
+});
 
-provide('field',computed(()=>{
+provide('field', computed(() => {
     return {
         ...props,
         invalid: !!props.error,
-        ariaDescribeBy:ariaDescribeBy.value
-    }
+        ariaDescribeBy: ariaDescribeBy.value
+    };
 }));
 </script>
