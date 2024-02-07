5285ff7f47a278221ea6ca6fa741506b8ca0cdd5
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-07 20:39:24 -0300

    export index para los componentes input

---
+++ frontend/src/components/input_components/index.js
@@ -0,0 +1,12 @@
+import Field from './Field.vue';
+import Input from './Input.vue';
+import TinyEditor from './TinyEditor.vue';
+import CoverImageInput from './CoverImageInput.vue';
+
+
+export {
+  Field,
+  Input,
+  TinyEditor,
+  CoverImageInput
+};
