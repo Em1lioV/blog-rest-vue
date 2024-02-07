71c79cd5a9ff60c8a7356bec79943b371071a094
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-07 20:41:38 -0300

    cambios menores skeltonTiny

--- frontend/src/components/TinyEditorSkeleton.vue
+++ frontend/src/components/TinyEditorSkeleton.vue
@@ -1,6 +1,6 @@
 <template>
      <div class="w-full h-[600px] bg-slate-100 opacity-90 animate-pulse">
-                    <div class="w-full min-h-[30px] max-h-[80px] my-[5px] bg-slate-300 opacity-70 flex-wrap animate-pulse flex justify-start items-center">
+                    <div class="w-full min-h-[30px] max-h-[80px] my-[5px] bg-slate-300 opacity-70 flex-nowrap sm:flex-wrap animate-pulse flex justify-start items-center">
                       <div class="min-w-[70px] w-[10%] h-[20px] bg-slate-100 m-2"></div>
                       <div class="min-w-[50px] w-[10%] h-[20px] bg-slate-100 m-2"></div>
                       <div class="min-w-[40px] w-[10%] h-[20px] bg-slate-100 m-2"></div>
@@ -8,7 +8,7 @@
                       <div class="min-w-[50px] w-[15%] bg-slate-100 m-2"></div>
                       
                     </div>
-                    <div class="w-full min-h-[30px] max-h-[80px] my-[5px] bg-slate-300 opacity-70 flex-wrap animate-pulse flex justify-start items-center">
+                    <div class="hidden sm:flex w-full min-h-[30px] max-h-[80px] my-[5px] bg-slate-300 opacity-70 flex-wrap animate-pulse justify-start items-center">
                       <div class="min-w-[70px] w-[10%] h-[20px] bg-slate-100 m-2"></div>
                       <div class="min-w-[50px] w-[10%] h-[20px] bg-slate-100 m-2"></div>
                       <div class="min-w-[50px] grow h-[20px] bg-slate-100 m-2"></div>
