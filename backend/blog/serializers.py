fad5362f5ec17e0fa0176a62b75a66e016156c07
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-18 16:22:52 -0300

    actualizacion django serializers

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -2,10 +2,18 @@ from rest_framework import serializers
 from .models import Post, User,Role
 from django.utils.html import strip_tags
 from html import unescape
+from django.db import IntegrityError
+from autoslug import AutoSlugField
 from rest_framework_simplejwt.exceptions import InvalidToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer,TokenRefreshSerializer
-from django.utils.text import slugify
+
 import re
+
+
+class RoleSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Role
+          fields = ['id','description']
 
 class CookieTokenRefreshSerializer(TokenRefreshSerializer):
     refresh = None
@@ -22,91 +30,52 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     }
     username_field = 'email'
 
+class UserShortSerializer(serializers.ModelSerializer):
+    role = RoleSerializer()
+    
+    class Meta:
+          model = User
+          fields = ['id','fullname','profile_image','initials','role']
+          
+    def get_fullname(self, obj):
+        return obj.fullname
+    
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
-        read_only_fields = ['author']
-    
-    def create(self, validated_data):
-        # Genera el slug basado en el título antes de crear el post
-        validated_data['slug'] = slugify(validated_data['title'])
-        return super().create(validated_data)
+        fields = ['id','title', 'excerpt', 'content', 'published', 'thumbnail','status', 'slug']
 
 class PostAuthorSerializer(serializers.ModelSerializer):
-    author_id = serializers.IntegerField(source='author.id', read_only=True)
-    author_name = serializers.CharField(source='author.name', read_only=True)
-    author_profile_image = serializers.CharField(source='author.profile_image', read_only=True)
-    author_role_description = serializers.CharField(source='author.role.description', read_only=True)
-
+    author = UserShortSerializer()
     class Meta:
         model = Post
-        fields = [
-            'title',
-            'excerpt',
-            'content',
-            'published',
-            'thumbnail',
-            'slug',
-            'author_id',
-            'author_name',
-            'author_profile_image',
-            'author_role_description',
-        ] 
-
-class CustomPostAuthorSerializer(PostAuthorSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'published', 'thumbnail', 'slug', 'excerpt','content','author_id',
-            'author_name',
-            'author_profile_image',
-            'author_role_description']
-    
-    def get_content(self, obj):
-        # Deshacerse de entidades HTML
-        cleaned_content = unescape(obj.content)
-        
-        # Agregar espacios entre los contenidos para evitar que se junten
-        cleaned_content = re.sub(r'(?<=>)([^<]*)(?=<)', r'\1 ', cleaned_content)
-
-        # Eliminar etiquetas HTML, limitar a 200 caracteres y ajustar espacios
-        cleaned_content = strip_tags(cleaned_content)[:200]
-        cleaned_content = re.sub(r'\s+', ' ', cleaned_content)
-
-        return cleaned_content
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        # Sobrescribir el valor de content en la representación
-        representation['content'] = self.get_content(instance)
-        return representation
+        fields = ['id','title', 'excerpt', 'content', 'published', 'thumbnail','status', 'slug','author']
 
 
 class UserSerializer(serializers.ModelSerializer):
-     role_description = serializers.ReadOnlyField(source='role.description')
-     
-     class Meta:
-          model = User
-          fields = ['id','name','email','profile_image', 'password','role','role_description']
-          extra_kwargs = {
-               'password': {'write_only':True}
-          }
-     
-     def create(self, validated_data):
-          password = validated_data.pop('password',None)
-          instance = self.Meta.model(**validated_data)
-          if password is not None:
-               instance.set_password(password)
-          instance.save()
-          return instance
+    role = RoleSerializer()
+    fullname = serializers.SerializerMethodField()
 
-class UserShortSerializer(serializers.ModelSerializer):
-     class Meta:
-          model = User
-          fields = ['id','name','profile_image']
+    class Meta:
+        model = User
+        fields = ['id', 'firstName', 'lastName', 'initials', 'email', 'profile_image', 'password', 'role', 'fullname']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        try:
+            instance.save()
+        except IntegrityError:
+            raise serializers.ValidationError({'email': ['El usuario con este correo electrónico ya existe.']})
+        return instance
+
+    def get_fullname(self, obj):
+        return obj.fullname
+    
 
 
-class RoleSerializer(serializers.ModelSerializer):
-     class Meta:
-          model = Role
-          fields = ['id','description']
