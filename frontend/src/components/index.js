7a64be136c770687b888d0a8a4c6898f5972fb2e
Em1lioV <emilio.villagrawt@gmail.com>
2024-02-21 19:40:24 -0300

    añadido component index for exports

---
+++ frontend/src/components/index.js
@@ -0,0 +1,19 @@
+
+import NavBar from './NavBar.vue';
+import Avatar from './Avatar.vue';
+import Dropdown from './Dropdown.vue';
+import Post from './Post.vue';
+import PostCard from './PostCard.vue';
+import PostList from './PostList.vue';
+import TinyEditorSkeleton from './TinyEditorSkeleton.vue';
+
+
+export {
+  NavBar,
+  Avatar,
+  Dropdown,
+  Post,
+  PostCard,
+  PostList,
+  TinyEditorSkeleton
+};
